20e3c00f8ac0d0cdce3cf77af7fe1c5258bca258
Randy Damron <rdamron@users.noreply.github.com>
2016-10-09 02:26:11 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,27 +1,43 @@
 local reactor = peripheral.wrap("BigReactors-Reactor_0")
 local monitor = peripheral.wrap("top")
+local bank = peripheral.wrap("tile_blockcapacitorbank_name_0")
 
 local low = 1000000
 local high = 9000000
 
-monitor.setTextScale(5)
+local function updateScreen()
+    monitor.setTextScale(5)
+    monitor.clear()
 
-while true do
-  monitor.clear()
-  
-  monitor.setCursorPos(1,1)
-  monitor.write( reactor.isActive() )
-  
-  monitor.setCursorPos(1,2)
-  monitor.write( reactor.getEnergyStored() )
-  
-  if reactor.getEnergyStored() <= low then
-    reactor.setActive(true)
-  end
- 
-  if reactor.getEnergyStored() >= high then
-    reactor.setActive(false)
-  end
- 
-  sleep(1)
+    monitor.setCursorPos(1,1)
+    monitor.write( reactor.isActive() )
+
+    monitor.setCursorPos(1,2)
+    monitor.write( reactor.getEnergyStored() )
+
+    monitor.setCursorPos(1,4)
+    monitor.write( bank.getMaxEnergyStored() )
+
+    monitor.setCursorPos(1,5)
+    monitor.write( bank.getEnergyStored() )
 end
+
+local function main()
+  
+  while true do
+    updateScreen()
+    
+    if reactor.getEnergyStored() <= low then
+      reactor.setActive(true)
+    end
+
+    if reactor.getEnergyStored() >= high then
+      reactor.setActive(false)
+    end
+
+    sleep(1)
+  end
+  
+end
+
+main()
