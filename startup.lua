dda7310c069b024d8d1d543613fc322be44f1bdd
Randy Damron <rdamron@users.noreply.github.com>
2016-10-09 01:41:37 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,14 +1,23 @@
-local reactor = peripheral.wrap("BigReactors-Reactor_1")
+local reactor = peripheral.wrap("BigReactors-Reactor_0")
+local monitor = peripheral.wrap("top")
+
 local low = 1000000
 local high = 9000000
 
+monitor.setTextScale(5)
+
 while true do
+  monitor.clear()
+  monitor.setCursorPos(1,1)
+  
+  monitor.write( reactor.isActive() )
+  
   if reactor.getEnergyStored() <= low then
-    reactor.setAllControlRodLevels(0)
+    reactor.setActive(true)
   end
  
   if reactor.getEnergyStored() >= high then
-    reactor.setAllControlRodLevels(100)
+    reactor.setActive(false)
   end
  
   sleep(1)
